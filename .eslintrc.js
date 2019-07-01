b661d323c510d80860018d2ecb64ed918bde7f9c
Justin Kaldenbach <kaldejm@gmail.com>
2019-07-01 14:00:30 -0400

    configure linting and formatter

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["standard", "prettier"],
+  globals: {
+    after: false,
+    afterEach: false,
+    assert: false,
+    before: false,
+    beforeEach: false,
+    describe: false,
+    expect: false,
+    it: false,
+    request: false
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  quotes: true
+};
